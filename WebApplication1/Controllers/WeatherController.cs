c7fec38106f4e346ad18ed054b195ac2b0b10ff9
hzt <ga503306@yahoo.com.tw>
2020-07-28 14:55:06 +0800

    Initial commit

---
+++ WebApplication1/Controllers/WeatherController.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class WeatherController : Controller
+    {
+        // GET: Weather
+        public ActionResult Index()
+        {
+            
+            List<Weather> Weathers = new List<Weather>();
+            int index = 0;
+            string state = ""; //英文描述
+            string state_chs = ""; //中文描述
+            string locationName = ""; //中文地名
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("C:\\Users\\ga503\\Downloads\\F-D0047-065.xml");
+            XmlNamespaceManager xnm = new XmlNamespaceManager(xmlDoc.NameTable);
+            xnm.AddNamespace("pf", "urn:cwb:gov:tw:cwbcommon:0.1");
+            XmlNode xn = xmlDoc.SelectSingleNode("//pf:cwbopendata/pf:dataset/pf:locations", xnm);
+            XmlNodeList xnlA = xn.ChildNodes;
+            foreach (XmlNode xnA in xnlA)
+            {
+                XmlElement xeB = (XmlElement)xnA;
+                XmlNodeList xnlB = xeB.ChildNodes;
+                List<Weather_detail> Weather_details = new List<Weather_detail>();
+                foreach (XmlNode xnB in xnlB)
+                {
+                    if (xnB.Name == "locationName")
+                    {
+                        XmlElement xeC = (XmlElement)xnB;
+                        XmlNodeList xnlC = xeC.ChildNodes;
+                        locationName = xnlC[0].InnerText;//地名暫存
+                    }
+                    if (xnB.Name == "weatherElement")
+                    {
+                        XmlElement xeC = (XmlElement)xnB;
+                        XmlNodeList xnlC = xeC.ChildNodes;
+                        foreach (XmlNode xnC in xnlC)
+                        {
+                            if (xnC.Name == "elementName")
+                            {
+                                state = xnC.InnerText;
+                            }
+                            else if (xnC.Name == "description")
+                            {
+                                state_chs = xnC.InnerText;
+                            }
+                            else
+                            {
+                                
+                                if (xnC.Name == "time" && state == "T")
+                                {
+                                    XmlElement xeD = (XmlElement)xnC;
+                                    XmlNodeList xnlD = xeD.ChildNodes;
+                                    Weather_details.Add(new Weather_detail { 時間 = xnlD[0].InnerText, 溫度 = xnlD[1].InnerText });
+                                }
+                                if (xnC.Name == "time" && state == "Td")
+                                {
+                                    XmlElement xeD = (XmlElement)xnC;
+                                    XmlNodeList xnlD = xeD.ChildNodes;
+                                    index = Weather_details.FindIndex(x =>(x.時間 == xnlD[0].InnerText));
+                                    Weather_details[index].露點溫度 = xnlD[1].InnerText;
+                                }
+                                if (xnC.Name == "time" && state == "RH")
+                                {
+                                    XmlElement xeD = (XmlElement)xnC;
+                                    XmlNodeList xnlD = xeD.ChildNodes;
+                                    index = Weather_details.FindIndex(x =>  (x.時間 == xnlD[0].InnerText));
+                                    Weather_details[index].相對溼度 = xnlD[1].InnerText;
+                                }
+                                if (xnC.Name == "time" && state == "PoP6h")
+                                {
+                                    XmlElement xeD = (XmlElement)xnC;
+                                    XmlNodeList xnlD = xeD.ChildNodes;
+                                    index = Weather_details.FindIndex(x =>  (x.時間 == xnlD[0].InnerText));
+                                    Weather_details[index].六小時降雨機率 = xnlD[2].InnerText;
+                                }
+                                if (xnC.Name == "time" && state == "PoP12h")
+                                {
+                                    XmlElement xeD = (XmlElement)xnC;
+                                    XmlNodeList xnlD = xeD.ChildNodes;
+                                    index = Weather_details.FindIndex(x =>  (x.時間 == xnlD[0].InnerText));
+                                    Weather_details[index].十二小時降雨機率 = xnlD[2].InnerText;
+                                }
+                                if (xnC.Name == "time" && state == "WD")
+                                {
+                                    XmlElement xeD = (XmlElement)xnC;
+                                    XmlNodeList xnlD = xeD.ChildNodes;
+                                    index = Weather_details.FindIndex(x =>  (x.時間 == xnlD[0].InnerText));
+                                    Weather_details[index].風向 = xnlD[1].InnerText;
+                                }
+                                if (xnC.Name == "time" && state == "WS")
+                                {
+                                    XmlElement xeD = (XmlElement)xnC;
+                                    XmlNodeList xnlD = xeD.ChildNodes;
+                                    index = Weather_details.FindIndex(x => (x.時間 == xnlD[0].InnerText));
+                                    Weather_details[index].風速 = xnlD[1].InnerText;
+                                }
+                                if (xnC.Name == "time" && state == "CI")
+                                {
+                                    XmlElement xeD = (XmlElement)xnC;
+                                    XmlNodeList xnlD = xeD.ChildNodes;
+                                    index = Weather_details.FindIndex(x =>  (x.時間 == xnlD[0].InnerText));
+                                    Weather_details[index].舒適度指數 = xnlD[1].ChildNodes[0].InnerText + xnlD[2].ChildNodes[0].InnerText;
+                                }
+                                if (xnC.Name == "time" && state == "AT")
+                                {
+                                    XmlElement xeD = (XmlElement)xnC;
+                                    XmlNodeList xnlD = xeD.ChildNodes;
+                                    index = Weather_details.FindIndex(x =>  (x.時間 == xnlD[0].InnerText));
+                                    Weather_details[index].體感溫度 = xnlD[1].InnerText;
+                                }
+                                if (xnC.Name == "time" && state == "Wx")
+                                {
+                                    XmlElement xeD = (XmlElement)xnC;
+                                    XmlNodeList xnlD = xeD.ChildNodes;
+                                    index = Weather_details.FindIndex(x =>  (x.時間 == xnlD[0].InnerText));
+                                    Weather_details[index].天氣現象 = xnlD[2].ChildNodes[0].InnerText;
+                                }
+                                if (xnC.Name == "time" && state == "WeatherDescription")
+                                {
+                                    XmlElement xeD = (XmlElement)xnC;
+                                    XmlNodeList xnlD = xeD.ChildNodes;
+                                    index = Weather_details.FindIndex(x =>  (x.時間 == xnlD[0].InnerText));
+                                    Weather_details[index].天氣預報綜合描述 = xnlD[2].ChildNodes[0].InnerText;
+                                }
+                            }
+                        }
+                       
+                    }
+                    
+                }
+                Weathers.Add(new Weather { 地點 = locationName, Weather_detail = Weather_details });
+                //Weather_details.Clear();
+            }
+            Weathers.RemoveAt(0);
+            ViewData["weathers"] = Weathers;
+            ViewBag.weather = Weathers;
+            return View();
+        }
+
+        // GET: Weather/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Weather/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Weather/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Weather/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Weather/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Weather/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Weather/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication1/Models/Weather.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Weather
+    {
+        public string 地點 { get; set; }
+        public List<Weather_detail> Weather_detail { get; set; }
+
+    }
+
+    public class Weather_detail
+    {
+        public string 時間 { get; set; }
+        public string 溫度 { get; set; }
+        public string 露點溫度 { get; set; }
+        public string 相對溼度 { get; set; }
+        public string 六小時降雨機率 { get; set; }
+        public string 十二小時降雨機率 { get; set; }
+        public string 風向 { get; set; }
+        public string 風速 { get; set; }
+        public string 舒適度指數 { get; set; }
+        public string 體感溫度 { get; set; }
+        public string 天氣現象 { get; set; }
+        public string 天氣預報綜合描述 { get; set; }
+    }
+}
